10ea1530dc57befd82548cedccd1b033010442ac
kanosaki <kanosaki@gmail.com>
2012-10-25 23:43:21 +0900

    Add GraphicsAdapter

---
+++ src/main/java/adsim/misc/GraphicsAdapter.java
@@ -0,0 +1,36 @@
+package adsim.misc;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import lombok.Delegate;
+
+public class GraphicsAdapter {
+
+    @Delegate(types = Graphics2D.class)
+    private final Graphics2D g;
+
+    public GraphicsAdapter(Graphics gc) {
+        this(gc, true);
+    }
+
+    public GraphicsAdapter(Graphics gc, boolean enableAntiAlias) {
+        g = (Graphics2D) gc;
+        if (enableAntiAlias)
+            this.setAitiAliasing();
+    }
+    
+    public static GraphicsAdapter create(Graphics g) {
+        return new GraphicsAdapter(g);
+    }
+
+    public void setAitiAliasing() {
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    }
+
+    public void drawOval(double x, double y, double width, double height) {
+        
+    }
+
+}
