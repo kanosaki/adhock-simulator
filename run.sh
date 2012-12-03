cd9a79908a70f6a5ae2276cb93379b96fd35bf13
kanosaki <kanosaki@gmail.com>
2012-12-03 09:31:34 +0900

    Modify run.sh: Assign more memory space to execute.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 if [[ $# -lt 2 ]]; then
-    java -Xmx2048m -server -jar ./target/adsim.jar 100:1000.0:3:regular:1000:0.01 2
+    java -Xmx4906m -server -jar ./target/adsim.jar 100:1000.0:3:regular:1000:0.01 2
 else
-    java -Xmx2048m -server -jar ./target/adsim.jar $*
+    java -Xmx4906m -server -jar ./target/adsim.jar $*
 fi 
